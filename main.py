c7646aadbdd36ad1c7b730e0f6efb09e415276d5
Harshit Nath <52531380+itsmehoney07@users.noreply.github.com>
2023-07-04 17:20:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import mlflow
-
 def entry_point(parameters):
   """Entry point for the MLflow project."""
-  # Log a metric.
-  mlflow.log_metric("accuracy", 0.99)
-  # Log an artifact.
-  mlflow.log_artifact("model.pkl")
+  # Do something with the parameters.
+  # Log metrics and artifacts.
   return
-
-if __name__ == "__main__":
-  mlflow.run(".", entry_point=entry_point)
